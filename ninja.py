015230bfb0ee0b0d6ffb732dac0a5ae99bd74917
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-04-30 22:32:00 +0530

    timer added and random fruit motion screening

--- ninja.py
+++ ninja.py
@@ -27,3 +27,42 @@ font = pygame.font.Font(os.path.join(os.getcwd(), 'comic.ttf'), 42)
 score_text = font.render('Score : ' + str(score), True, (255, 255, 255))  # score display
 lives_icon = pygame.image.load('images/white_lives.png')  # images that shows remaining lives
 
+
+# Timer
+game_time = 30  # seconds
+start_time = pygame.time.get_ticks()
+
+# Generalized structure of the fruit Dictionary
+def generate_random_fruits(fruit):
+    fruit_path = "images/" + fruit + ".png"
+    data[fruit] = {
+        'img': pygame.image.load(fruit_path),
+        'x': random.randint(100, 500),  # where the fruit should be positioned on x-coordinate
+        'y': 800,
+        'speed_x': random.randint(-10, 10),
+        # how fast the fruit should move in x direction. Controls the diagonal movement of fruits
+        'speed_y': random.randint(-80, -60),  # control the speed of fruits in y-directionn ( UP )
+        'throw': False,
+        # determines if the generated coordinate of the fruits is outside the gameDisplay or not. If outside, then it will be discarded
+        't': 0,  # manages the
+        'hit': False,
+    }
+
+    if random.random() >= 0.75:  # Return the next random floating point number in the range [0.0, 1.0) to keep the fruits inside the gameDisplay
+        data[fruit]['throw'] = True
+    else:
+        data[fruit]['throw'] = False
+
+
+# Dictionary to hold the data the random fruit generation
+data = {}
+for fruit in fruits:
+    generate_random_fruits(fruit)
+
+
+def hide_cross_lives(x, y):
+    gameDisplay.blit(pygame.image.load("images/red_lives.png"), (x, y))
+
+
+# Generic method to draw fonts on the screen
+font_name = pygame.font.match_font('comic.ttf')
