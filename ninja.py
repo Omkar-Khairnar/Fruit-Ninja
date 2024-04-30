c353e6f55501dc12c7c28d82921a2ae18c6eb9aa
Saurabh Rawat <yores.2002@gmail.com>
2024-04-30 22:28:40 +0530

    start screen

--- ninja.py
+++ ninja.py
@@ -1,2 +1,29 @@
+import pygame
+import os
+import random
 
-import pygame, sys
+player_lives = 3  # keep track of lives
+score = 0  # keeps track of score
+fruits = ['melon', 'orange', 'pomegranate', 'guava', 'bomb']  # entities in the game
+
+# initialize pygame and create window
+WIDTH = 800
+HEIGHT = 500
+FPS = 12  # controls how often the gameDisplay should refresh. In our case, it will refresh every 1/12th second
+pygame.init()
+pygame.display.set_caption('Fruit-Ninja Game -- Python.hunt')
+gameDisplay = pygame.display.set_mode((WIDTH, HEIGHT))  # setting game display size
+clock = pygame.time.Clock()
+
+# Define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+background = pygame.image.load('back.jpg')  # game background
+font = pygame.font.Font(os.path.join(os.getcwd(), 'comic.ttf'), 42)
+score_text = font.render('Score : ' + str(score), True, (255, 255, 255))  # score display
+lives_icon = pygame.image.load('images/white_lives.png')  # images that shows remaining lives
+
